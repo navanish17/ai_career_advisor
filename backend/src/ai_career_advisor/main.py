04bf68c61bfe4c1555e02d99819cfe27f4f8a5a4
navanish17 <navnishpande17@gmail.com>
2025-12-10 19:17:46 +0530

    phase 5 step 5.0 profile module

--- backend/src/ai_career_advisor/main.py
+++ backend/src/ai_career_advisor/main.py
@@ -9,14 +9,14 @@ app = create_app()
 
 #include routers
 
-app.include_router(auth.router, prefix = "/api/auth")
-app.include_router(agent.router, prefix = "/api/agent")
-app.include_router(career.router, prefix = "/api/career")
-app.include_router(colleges.router, prefix = "/api/colleges")
-app.include_router(profile.router, prefix = "/api/profile")
-app.include_router(quiz.router, prefix = "/api/quiz")
-app.include_router(roadmap.router, prefix = "/api/roadmap")
-app.include_router(scholarships.router, prefix = "/api/scholarships")
+app.include_router(auth, prefix="/api/auth")
+app.include_router(agent, prefix="/api/agent")
+app.include_router(career, prefix="/api/career")
+app.include_router(colleges, prefix="/api/colleges")
+app.include_router(profile, prefix="/api/profile")
+app.include_router(quiz, prefix="/api/quiz")
+app.include_router(roadmap, prefix="/api/roadmap")
+app.include_router(scholarships, prefix="/api/scholarships")
 
 @app.get('/health')
 async def health_check():
--- backend/src/ai_career_advisor/models/profile.py
+++ backend/src/ai_career_advisor/models/profile.py
@@ -1,28 +1,23 @@
-from sqlalchemy import Column, String, Integer, ForeignKey,JSON, DateTime,func
+from sqlalchemy import Column, String, Integer, ForeignKey, JSON, DateTime, func
 from sqlalchemy.orm import relationship
 from ai_career_advisor.core.database import Base
 
-class profile(Base):
+class Profile(Base):
     __tablename__ = "profiles"
 
-    id = Column(Integer, primary_key = True, index= True)
+    id = Column(Integer, primary_key=True, index=True)
 
-    user_id = Column(Integer, ForeignKey("users.id", ondelete = "CASCADE"), unique = True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=True)
 
-    class_level = Column(String, nullable = True)
-    location = Column(String, nullable = True)
-    language = Column(String, nullable = True)
+    class_level = Column(String, nullable=True)
+    location = Column(String, nullable=True)
+    language = Column(String, nullable=True)
 
-    #Known interest
-
-    known_interest = Column(JSON, nullable=True)
+    known_interests = Column(JSON, nullable=True)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
 
-    # Relationship back to User
     user = relationship("User", back_populates="profile")
-
-
